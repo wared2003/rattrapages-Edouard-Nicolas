175a3a61bb5298b9194603017679bc8efab4743c
Edouard Nicolas <edouardn809@gmail.com>
2022-05-02 12:20:23 +0200

    [add] setup the tailwind config

--- 02 – Architecture front-end/tailwind.config.js
+++ 02 – Architecture front-end/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': '#FBAF41',
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'grey': '#646464',
+        'light-grey': '#646464',
+        'bcg-grey': '#DCDCDC',
+        'bcg-lightgrey': '#ECECEC',
+        'green': '#2DD393',
+        'green-1': '#5ABFAB',
+        'green-2': '#56AE9C',
+        'green-3': '#42AA94',
+        'green-4': '#379D88',
+        'green-5': '#298572',
+      }
+    },
   },
   plugins: [],
 }
